af6c7423f3da647ff45105584f3916d6453507fb
enoch harnanto <enochharnantoo@gmail.com>
2022-04-12 15:13:50 -0700

    Add nodemon dependency and sample data

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const messages = [
+    {
+      text: "Hi there!", 
+      user: "Adam",
+      added: new Date()
+    },
+    {
+      text: "Hello world!", 
+      user: "Charles",
+      added: new Date()
+    }
+  ];
+
   res.render('index', { title: 'Express' });
 });
 
