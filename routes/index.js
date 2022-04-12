087e7a4870df31e64f9ce44e71bc8167108f9f2a
enoch harnanto <enochharnantoo@gmail.com>
2022-04-12 15:29:51 -0700

    Display sample data

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!", 
+    user: "Adam",
+    added: new Date()
+  },
+  {
+    text: "Hello world!", 
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!", 
-      user: "Adam",
-      added: new Date()
-    },
-    {
-      text: "Hello world!", 
-      user: "Charles",
-      added: new Date()
-    }
-  ];
-
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages: messages });
 });
 
 module.exports = router;
