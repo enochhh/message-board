eba25c1d1399ccd16ca8fc449b830485c275d0cb
enoch harnanto <enochharnantoo@gmail.com>
2022-04-14 10:33:18 -0700

    Added one more sample post

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -1,8 +1,15 @@
 const moment = require('moment');
 const date1 = moment().startOf('hour').fromNow();
 const date2 = moment().startOf('minute').fromNow();
+const date3 = moment().startOf('hour').fromNow();
 
 const messages = [
+    {
+      text: "Data structures and algos grind",
+      user: "Josie",
+      description: "I'm having a hard time with algorithms :( Leetcode's got me stumped",
+      added: date2
+    },
     {
       text: "I hate frontend stuff", 
       user: "Adam",
@@ -13,7 +20,7 @@ const messages = [
       text: "I love doing frontend stuff!", 
       user: "Charles",
       description: "I started learning HTML, CSS, and JS a couple months ago and it's been fun! Just picked up React two weeks ago and that's been great! I love seeing the work I do get rendered on the page; it's super rewarding.",
-      added: date2
+      added: date3
     }
   ];
 
